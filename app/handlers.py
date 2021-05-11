e82400e7154f36b794a6d9c4e92e93c93204a258
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-05-11 23:15:19 +0700

    добавил хэндлер для /help

--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,16 @@
 from aiogram import types
 from aiogram.dispatcher import Dispatcher
+from app.config import START_MESSAGE, HELP_MESSAGE
 
 
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+async def start_command(message: types.Message):
+    await message.answer(START_MESSAGE)
+
+
+async def help_command(message: types.Message):
+    await message.answer(HELP_MESSAGE)
 
 
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(process_start_command, commands=['start'])
+    dp.register_message_handler(start_command, commands=['start'])
+    dp.register_message_handler(help_command, commands=['help'])
