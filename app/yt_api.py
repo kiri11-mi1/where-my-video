7be89d1c87baaa6f97b4668984af6898e2a40346
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-05-16 16:42:11 +0700

    убрал лишние методы

--- app/yt_api.py
+++ app/yt_api.py
@@ -1,6 +1,6 @@
 from youtube_api import YoutubeDataApi
 from app.config import YT_TOKEN
-
+import re
 
 class YTApi(YoutubeDataApi):
 
@@ -8,9 +8,8 @@ class YTApi(YoutubeDataApi):
         return self.yt.get_video_metadata(video_id)['video_title']
 
 
-    def get_videos_from_channel(self, channel_id: str) -> list:
-        main_playlist_id = self.get_channel_metadata(channel_id=channel_id)['playlist_id_uploads']
-        return self.get_videos_from_playlist_id(playlist_id=main_playlist_id)
+    def get_channel_name(self, channel_id):
+        return self.get_channel_metadata(channel_id)['title']
 
 
     def get_last_video_id(self, channel_id):
@@ -19,23 +18,36 @@ class YTApi(YoutubeDataApi):
         return all_videos[0]['video_id']
 
 
-    def get_channel_id(self, channel_url) -> str:
-        pass
-
-
-    def get_channel_id_by_url(self, chan_url:str) -> str:
-        if result_search := self.search(chan_url):
-            return result_search[0]['channel_id']
+    def get_channel_id_by_url(self, channel_url:str) -> str:
+        channel_id = re.split('/+', channel_url)[-1]
+        if self.get_channel_metadata(channel_id):
+            return channel_id
 
 
 if __name__ == '__main__':
+    # response = requests.get(
+    #     url = f'https://www.googleapis.com/youtube/v3/channels?part=snippetforUsername=ExtremeCode&key={YT_TOKEN}',
+    #     # params = {
+    #     #     'part': 'snippetforUsername=ExtremeCode',
+    #     #     'key': YT_TOKEN
+    #     # }
+    # ).json()
+    # print(response)
     # ExtremeCode - Channel Id - UCBNlINWfd08qgDkUTaUY4_w
     yt = YTApi(YT_TOKEN)
-    url = [
-        'https://www.youtube.com/channel/UCBNlINWfd08qgDkUTaUY4_w',
-        'https://www.youtube.com/c/ExtremeCode',
-        'https://www.youtube.com/channel/UCAJYuvFKA_MlFq6ekkn7WFA'
-    ]
+    # result = yt.search('https://www.youtube.com/channel/UCBNlINWfd08qgDkUTaUY4_w')
+
+    # url = [
+    #     'https://www.youtube.com/channel/UCBNlINWfd08qgDkUTaUY4_w',
+    #     'https://www.youtube.com/c/ExtremeCode',
+    #     'https://www.youtube.com/channel/UCAJYuvFKA_MlFq6ekkn7WFA'
+    # ]
     
-    result = yt.get_last_video_id('UCBNlINWfd08qgDkUTaUY4_w')
-    print(result)
+    
+    # # result = yt.get_last_video_id('UCBNlINWfd08qgDkUTaUY4_w')
+    # result = yt.get_channel_metadata(channel_id='ExtremeCode')
+    # result = yt.get_channel_id('https://www.youtube.com/channel/UCqqISS-PyCnhjbpXqhclvaQ')
+    # result = yt.get_last_video_id('UCBNlINWfd08qgDkUTaUY4_w')
+    # print(result)
+    # UCD5_waDcGBhof9xuA1qovTQ
+    # UCPJXxmrw1NRmOwdZtLM694g
