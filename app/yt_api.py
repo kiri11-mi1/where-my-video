e0060309cfcc6d80d0724b54960fd0a622238d9a
Kirill <kirill.mil.0310@mail.ru>
2021-05-22 22:49:43 +0700

    добавил поиск id канала по short url

--- app/yt_api.py
+++ app/yt_api.py
@@ -1,19 +1,16 @@
 from youtube_api import YoutubeDataApi
-from app.config import YT_TOKEN
+from config import YT_TOKEN
 import re
 import requests
 
 
 class YTApi(YoutubeDataApi):
 
-
     def video_title(self, video_id: str) -> str:
         return self.get_video_metadata(video_id)['video_title']
 
-
     def get_channel_name(self, channel_id):
         return self.get_channel_metadata(channel_id)['title']
-
 
     def get_last_video_id(self, channel_id):
         try:
@@ -23,14 +20,17 @@ class YTApi(YoutubeDataApi):
         except requests.exceptions.HTTPError:
             return None
 
-
-    def get_channel_id_by_url(self, channel_url:str) -> str:
+    def get_channel_id_by_url(self, channel_url: str) -> str:
         channel_id = re.split('/+', channel_url)[-1]
         if self.get_channel_metadata(channel_id):
             return channel_id
+        if channel_info := self.search(channel_id):
+            return channel_info[0]['channel_id']
 
 
 if __name__ == '__main__':
     yt = YTApi(YT_TOKEN)
-    result = yt.get_video_metadata('G6rrIxY9q0')
+    result = yt.get_channel_id_by_url(
+        'https://www.youtube.com/c/ArchakovBlog'
+    )
     print(result)
