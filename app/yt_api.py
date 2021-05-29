1b0c2d514f27c9c76ad57cac866d61eb497c8c6b
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-05-29 19:41:10 +0700

    добавил логгирование

--- app/yt_api.py
+++ app/yt_api.py
@@ -2,15 +2,28 @@ from youtube_api import YoutubeDataApi
 from app.config import YT_TOKEN
 import re
 import requests
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+)
 
 class YTApi(YoutubeDataApi):
 
     def video_title(self, video_id: str) -> str:
-        return self.get_video_metadata(video_id)['video_title']
+        try:
+            metadata = self.get_video_metadata(video_id)
+            return metadata['video_title']
+        except requests.exceptions.HTTPError:
+            logging.error('The number of requests has ended')
 
     def get_channel_name(self, channel_id):
-        return self.get_channel_metadata(channel_id)['title']
+        try:
+            metadata = self.get_channel_metadata(channel_id)
+            return metadata['title']
+        except requests.exceptions.HTTPError:
+            logging.error('The number of requests has ended')
 
     def get_last_video_id(self, channel_id):
         try:
@@ -18,14 +31,17 @@ class YTApi(YoutubeDataApi):
             all_videos = self.get_videos_from_playlist_id(playlist_id=main_playlist_id)
             return all_videos[0]['video_id']
         except requests.exceptions.HTTPError:
-            return None
+            logging.error('The number of requests has ended')
 
     def get_channel_id_by_url(self, channel_url: str) -> str:
-        channel_id = re.split('/+', channel_url)[-1]
-        if self.get_channel_metadata(channel_id):
-            return channel_id
-        if channel_info := self.search(channel_id):
-            return channel_info[0]['channel_id']
+        try:
+            channel_id = re.split('/+', channel_url)[-1]
+            if self.get_channel_metadata(channel_id):
+                return channel_id
+            if channel_info := self.search(channel_id):
+                return channel_info[0]['channel_id']
+        except requests.exceptions.HTTPError:
+            logging.error('The number of requests has ended') 
 
 
 if __name__ == '__main__':
